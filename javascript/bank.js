db9d7a39a31537d21bb2dbf7ca3081a343e2aaae
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-02-10 21:26:31 +0600

    third

--- javascript/bank.js
+++ javascript/bank.js
@@ -3,7 +3,7 @@ document.getElementById("submit").addEventListener('click',function(){
      const pass=document.getElementById("pass").value;
 
      if(emailV=="shaharia@gmail.com"&& pass=="112233"){
-         window.location.href="monnney.html";
+         window.location.href="https://shaharia1999.github.io/bank-system/monnney.html";
      }
      else{
          alert("enter valid  email and password")
