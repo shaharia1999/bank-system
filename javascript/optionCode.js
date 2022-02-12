e134eb66a2ff80b5f806c72a7a86edd79a884c81
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-02-12 19:00:00 +0600

    optional code

---
+++ javascript/optionCode.js
@@ -0,0 +1,55 @@
+
+
+// document.getElementById("diposit").addEventListener('click',function(){
+//     // terger element
+//     let depoInput= document.getElementById("dTaka");
+//         depoamout=parseFloat(depoInput.value);
+//      depoInput=depoInput.value;
+   
+//      let newdipo= document.getElementById("depoTaka");
+//      let curreentDipo=parseFloat(newdipo.innerText);
+//        let ubdatedipo=  parseFloat(curreentDipo+depoamout);
+//        //
+
+//         newdipo.innerText=ubdatedipo;
+
+//        //clear input
+//       // depoInput.value=" ";
+
+//        const stingBalance=document.getElementById("total");
+//        const prevbalance= parseFloat(stingBalance.innerText);
+//        const ubdatebalance=prevbalance+depoamout;
+//       //  depoamout
+//        stingBalance.innerText=ubdatebalance;
+
+// })
+// document.getElementById("WithDrow").addEventListener('click',function(){
+
+
+//       const withwrowInpute= document.getElementById("withwrowInpute");
+//     //   convert the value string to number
+//     // this is withwdrow part
+//       const withwrowInputeValue=parseFloat(withwrowInpute.value);
+//          const showwithdrow=document.getElementById("showWithdrow");
+//          const showwithdrowValue=parseFloat(showwithdrow.innerText);
+//           const showwithdrowtotal=showwithdrowValue+withwrowInputeValue;
+//           showwithdrow.innerText=showwithdrowtotal;
+
+
+//         //   this is the balance part
+//       const totalbalance= document.getElementById('total');
+//     //   convert the string to number
+//       const totalbalanceValue=parseFloat(totalbalance.innerText);
+  
+//     //   calculation
+//     const withDroaAfterTotal=totalbalanceValue-withwrowInputeValue;
+//     // set inner text
+//     totalbalance.innerText=withDroaAfterTotal;
+    
+
+//     // clear the input
+//     withwrowInpute.value=" ";
+// })
+// document.getElementById("log out").addEventListener('click',function(){
+//     window.location="https://shaharia1999.github.io/bank-system/";
+// })
